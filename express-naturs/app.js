63100b1555514d66e06a50dd80f075397765d0b7
Haruna-EZURE <burimofumofu@gmail.com>
2024-02-16 15:18:31 +0900

    feat: POSTリクエストの追加

--- express-naturs/app.js
+++ express-naturs/app.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const express = require("express");
+const bodyParser = require("body-parser"); // 追加
 
 const app = express();
+
+app.use(bodyParser.json()); // 追加
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -15,6 +18,26 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
